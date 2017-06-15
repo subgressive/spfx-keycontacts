bbb4cf2f0359863b66fb713e9367fffa4a7f74b0
subgressive <michael.dockray@gmail.com>
2017-06-16 08:54:11 +1000

    Non Reactive Property Pane & parameter listname enabled

--- src/webparts/keyContacts/KeyContactsWebPart.ts
+++ src/webparts/keyContacts/KeyContactsWebPart.ts
@@ -63,4 +63,8 @@ export default class KeyContactsWebPart extends BaseClientSideWebPart<IKeyContac
       ]
     };
   }
+
+  protected get disableReactivePropertyChanges(): boolean {
+    return true;
+  }
 }
--- src/webparts/keyContacts/components/KeyContacts.tsx
+++ src/webparts/keyContacts/components/KeyContacts.tsx
@@ -4,7 +4,7 @@ import { IKeyContactsProps } from './IKeyContactsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { SPHttpClient, ISPHttpClientConfiguration, SPHttpClientResponse } from '@microsoft/sp-http';
 
-import { ContactCards} from './ContactsCard';
+import { ContactCards } from './ContactsCard';
 
 export default class KeyContacts extends React.Component<IKeyContactsProps, any> {
   constructor() {
@@ -15,7 +15,7 @@ export default class KeyContacts extends React.Component<IKeyContactsProps, any>
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this._getContacts();
   }
 
@@ -30,10 +30,10 @@ export default class KeyContacts extends React.Component<IKeyContactsProps, any>
   private _getContacts() {
     var myListName = this.props.listName;
     var maxCount = this.props.itemCount;
-    console.log("maxCount="+maxCount);
-    console.log("myListName="+myListName);
-    var url = this.props.webUrl + "/_api/web/lists/GetByTitle('Contacts')/items?$top="+maxCount+"&$expand=Contact/Id&$select=Title,Contact/Id,Contact/EMail,Contact/FirstName,Contact/LastName,Contact/Title,Contact/WorkPhone,Contact/Department,Contact/JobTitle";
-    console.log("url="+url);
+    console.log("maxCount=" + maxCount);
+    console.log("myListName=" + myListName);
+    var url = this.props.webUrl + "/_api/web/lists/GetByTitle('" + myListName + "')/items?$top=" + maxCount + "&$expand=Contact/Id&$select=Title,Contact/Id,Contact/EMail,Contact/FirstName,Contact/LastName,Contact/Title,Contact/WorkPhone,Contact/Department,Contact/JobTitle";
+    console.log("url=" + url);
     this._getSPData(url)
       .then(data => {
         //console.log('get the data' + data);
@@ -49,4 +49,5 @@ export default class KeyContacts extends React.Component<IKeyContactsProps, any>
         return data.value;
       });
   }
+
 }
